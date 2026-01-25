d96e5ad69747a90a2e08174028731054c5bc8568
Tushar Khatiwada <152291006+walkinguy1@users.noreply.github.com>
2026-01-25 09:05:37 +0545

    extra optimization

--- portfolio/src/components/Projects.jsx
+++ portfolio/src/components/Projects.jsx
@@ -3,7 +3,6 @@ import { ProjectCard } from "./ProjectCard";
 import projImg1 from "../assets/img/project-img1.png";
 import projImg2 from "../assets/img/project-img2.png";
 import projImg3 from "../assets/img/project-img3.png";
-import colorSharp2 from "../assets/img/color-sharp2.png";
 import 'animate.css';
 import TrackVisibility from 'react-on-screen';
 
@@ -92,7 +91,7 @@ export const Projects = () => {
           </Col>
         </Row>
       </Container>
-      <img className="background-image-right" src={colorSharp2}></img>
+
     </section>
   )
 }
--- portfolio/src/components/skills.jsx
+++ portfolio/src/components/skills.jsx
@@ -6,7 +6,6 @@ import python from "../assets/img/python.png";
 import uiux from "../assets/img/uiux.png";
 import Carousel from 'react-multi-carousel';
 import 'react-multi-carousel/lib/styles.css';
-import colorSharp from "../assets/img/color-sharp.png"
 
 export const Skills = () => {
   const responsive = {
@@ -67,7 +66,6 @@ export const Skills = () => {
                 </div>
             </div>
         </div>
-        <img className="background-image-left" src={colorSharp} alt="Image" />
     </section>
   )
 }
